580b92a1f2e67091976fa6f1165b7bc0df05fe3f
kodinkat <me@karlkatewu.com>
2021-10-11 12:57:51 +0100

    Replaced auto-add people group default workflow with a simpler example

--- workflows/workflows.php
+++ workflows/workflows.php
@@ -11,11 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Disciple_Tools_Plugin_Starter_Template_Workflows {
 
-    private static $action_custom_people_group_connections = [
-        'id'    => 'starter_groups_00001_custom_action_people_group_connections',
-        'label' => 'Auto-Add People Group Connections'
-    ];
-
     /**
      * Disciple_Tools_Plugin_Starter_Template_Workflows The single instance of Disciple_Tools_Plugin_Starter_Template_Workflows.
      *
@@ -46,20 +41,6 @@ class Disciple_Tools_Plugin_Starter_Template_Workflows {
      */
     public function __construct() {
         add_filter( 'dt_workflows', [ $this, 'fetch_default_workflows_filter' ], 10, 2 );
-        add_filter( 'dt_workflows_custom_actions', function ( $actions ) {
-            $actions[] = (object) [
-                'id'        => self::$action_custom_people_group_connections['id'],
-                'name'      => self::$action_custom_people_group_connections['label'],
-                'displayed' => true // Within admin workflow builder view?
-            ];
-
-            return $actions;
-        }, 10, 1 );
-
-        add_action( self::$action_custom_people_group_connections['id'], [
-            $this,
-            'custom_action_people_group_connections'
-        ], 10, 1 );
     }
 
     public function fetch_default_workflows_filter( $workflows, $post_type ) {
@@ -80,6 +61,9 @@ class Disciple_Tools_Plugin_Starter_Template_Workflows {
             case 'groups':
                 $this->build_default_workflows_groups( $workflows );
                 break;
+            case 'starter_post_type':
+                $this->build_default_workflows_starter( $workflows );
+                break;
         }
 
         return $workflows;
@@ -89,19 +73,21 @@ class Disciple_Tools_Plugin_Starter_Template_Workflows {
     }
 
     private function build_default_workflows_groups( &$workflows ) {
-        $dt_fields = DT_Posts::get_post_field_settings( 'groups' );
+    }
+
+    private function build_default_workflows_starter( &$workflows ) {
+        $dt_fields = DT_Posts::get_post_field_settings( 'starter_post_type' );
 
         $workflows[] = (object) [
-            'id'         => 'starter_groups_00001',
-            'name'       => '[Starter] Auto-Adding People Group Connections',
+            'id'         => 'starter_00001',
+            'name'       => 'Starter Template Add Text On Creation',
             'enabled'    => false, // Can be enabled via admin view
-            'trigger'    => Disciple_Tools_Workflows_Defaults::$trigger_updated['id'],
+            'trigger'    => Disciple_Tools_Workflows_Defaults::$trigger_created['id'],
             'conditions' => [
-                Disciple_Tools_Workflows_Defaults::new_condition(
-                    Disciple_Tools_Workflows_Defaults::$condition_is_set,
+                Disciple_Tools_Workflows_Defaults::new_condition( Disciple_Tools_Workflows_Defaults::$condition_is_set,
                     [
-                        'id'    => 'members',
-                        'label' => $dt_fields['members']['name']
+                        'id'    => 'name',
+                        'label' => $dt_fields['name']['name']
                     ], [
                         'id'    => '',
                         'label' => ''
@@ -109,76 +95,17 @@ class Disciple_Tools_Plugin_Starter_Template_Workflows {
                 )
             ],
             'actions'    => [
-                Disciple_Tools_Workflows_Defaults::new_action(
-                    Disciple_Tools_Workflows_Defaults::$action_custom,
+                Disciple_Tools_Workflows_Defaults::new_action( Disciple_Tools_Workflows_Defaults::$action_update,
                     [
-                        'id'    => 'members', // Field to be updated or an arbitrary selection!
-                        'label' => $dt_fields['members']['name']
+                        'id'    => 'disciple_tools_plugin_starter_template_text',
+                        'label' => $dt_fields['disciple_tools_plugin_starter_template_text']['name']
                     ], [
-                        'id'    => self::$action_custom_people_group_connections['id'], // Action Hook
-                        'label' => self::$action_custom_people_group_connections['label']
+                        'id'    => 'Auto Filled By Workflow Engine',
+                        'label' => 'Auto Filled By Workflow Engine'
                     ]
                 )
             ]
         ];
-    }
-
-    /**
-     * Workflow custom action self-contained function to handle following
-     * use case:
-     *
-     * When adding a contact to a group, if the contact has people group
-     * connections, also add those connections to the group.
-     *
-     * @var    object
-     * @access public
-     * @since  1.11.0
-     */
-    public function custom_action_people_group_connections( $post ) {
-        // Ensure post is a valid group type
-        if ( ! empty( $post ) && ( $post['post_type'] === 'groups' ) ) {
-
-            $new_members                      = [];
-            $new_members['members']['values'] = [];
-
-            // Iterate over group members in search of member connections to add to group
-            $members = $post['members'] ?? [];
-            foreach ( $members as $member ) {
-
-                if ( ! empty( $member ) && $member['post_type'] === 'contacts' ) {
-
-                    // Fetch member contacts record and any associated people group connections
-                    $member_post = DT_Posts::get_post( $member['post_type'], $member['ID'], false, false, false );
-                    if ( ! empty( $member_post ) && ! is_wp_error( $member_post ) && isset( $member_post['relation'] ) ) {
-
-                        foreach ( $member_post['relation'] as $connection ) {
-
-                            // Ensure connection is not already a group member -> safeguard against infinite post update loops!
-                            if ( ! $this->is_group_member( $members, $connection['ID'] ) ) {
-
-                                // Prepare non-group member connection for group addition
-                                $new_members['members']['values'][] = [ "value" => $connection['ID'] ];
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Assuming we have updated fields, proceed with post update!
-            if ( ! empty( $new_members['members']['values'] ) ) {
-                DT_Posts::update_post( $post['post_type'], $post['ID'], $new_members, false, false );
-            }
-        }
-    }
-
-    private function is_group_member( $members, $id ): bool {
-        foreach ( $members as $member ) {
-            if ( intval( $member['ID'] ) === intval( $id ) ) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
 

421575461b5b385bb6660ffeaae5e675150b1c3b
Prykon <dariomanoukian@gmail.com>
2022-05-09 20:38:00 -0300

    Script is stable

--- customize.sh
+++ customize.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
+plugin_name="My Awesome Plugin"
+plugin_class="My_Awesome_Plugin"
+plugin_function="my_awesome_plugin"
+plugin_path="my-awesome-plugin"
+plugin_post_type="awesome_plugin"
 
-plugin_name="Plugin Starter Template"
-plugin_class="Disciple_Tools_Plugin_Starter_Template"
-plugin_function="disciple_tools_plugin_starter_template"
-plugin_path="disciple-tools-plugin-starter-template"
-plugin_post_type="starter_post_type"
+echo -e "\nReplacing string 'Plugin Starter Template'... for '$plugin_name'."
+grep -rl --exclude-dir=*.git "Plugin Starter Template" | grep -Ev customize.sh | LANG=C xargs sed -i '' "s/Plugin Starter Template/${plugin_name}/g"
 
-
-echo -e "\nReplacing string 'Plugin Starter Template'... to '$plugin_name'."
-grep -rl "Plugin Starter Template" | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_name/g"
-#sed -i -- "s/Plugin Starter Template/$plugin_name/g" *.php
-
-echo -e "Replacing string 'Disciple_Tools_Plugin_Starter_Template'... to '$plugin_class'."
-grep -rl "Disciple_Tools_Plugin_Starter_Template" | grep -Ev customize.sh | xargs sed -i -- "s/Disciple_Tools_Plugin_Starter_Template/$plugin_class/g"
+echo -e "Replacing string 'Disciple_Tools_Plugin_Starter_Template'... for '$plugin_class'."
+grep -rl --exclude-dir=*.git "Disciple_Tools_Plugin_Starter_Template" | grep -Ev customize.sh | LANG=C xargs sed -i '' "s/Disciple_Tools_Plugin_Starter_Template/${plugin_class}/g"
 
 
-echo -e "Replacing string 'disciple_tools_plugin_starter_template'... to '$plugin_function'."
-grep -rl "disciple_tools_plugin_starter_template" | grep -Ev customize.sh | xargs sed -i -- "s/disciple_tools_plugin_starter_template/$plugin_function/g"
+echo -e "Replacing string 'disciple_tools_plugin_starter_template'... for '$plugin_function'."
+grep -rl --exclude-dir=*.git "disciple_tools_plugin_starter_template" | grep -Ev customize.sh | LANG=C xargs sed -i '' "s/disciple_tools_plugin_starter_template/${plugin_function}/g"
 
-echo -e "Replacing string 'disciple-tools-plugin-starter-template'... to '$plugin_path'."
-grep -rl "disciple-tools-plugin-starter-template" | grep -Ev customize.sh | xargs sed -i -- "s/disciple-tools-plugin-starter-template/$plugin_path/g"
+echo -e "Replacing string 'disciple-tools-plugin-starter-template'... for '$plugin_path'."
+grep -rl --exclude-dir=*.git "disciple-tools-plugin-starter-template" | grep -Ev customize.sh | LANG=C xargs sed -i '' "s/disciple-tools-plugin-starter-template/${plugin_path}/g"
 
-echo -e "Replacing string 'starter_post_type'... to '$plugin_post_type'."
-grep -rl "starter_post_type" | grep -Ev customize.sh | xargs sed -i -- "s/starter_post_type/$plugin_post_type/g"
+echo -e "Replacing string 'starter_post_type'... for '$plugin_post_type'."
+grep -rl --exclude-dir=*.git "starter_post_type" | grep -Ev customize.sh | LANG=C xargs sed -i '' "s/starter_post_type/${plugin_post_type}/g"
 
+echo -e "\nMoving disciple-tools-plugin-starter-template.php to '$plugin_path.php'"
 mv disciple-tools-plugin-starter-template.php "$plugin_path.php"
 
-echo -e "\n\nDone! Thanks for choosing Disciple.Tools."
+echo -e "\nAll replacements done. Destroying this script since it can be harmful if you re-run it."
+rm customize.sh
+
+echo -e "\nThanks for choosing Disciple.Tools!"

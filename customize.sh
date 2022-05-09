cc5b06c0bb508a6c908b71143d165d47d91c516c
Prykon <dariomanoukian@gmail.com>
2022-05-09 19:18:43 -0300

    Fixed Hardcoded variables;Removed test variable

--- customize.sh
+++ customize.sh
@@ -6,7 +6,6 @@ plugin_class="Disciple_Tools_Plugin_Starter_Template"
 plugin_function="disciple_tools_plugin_starter_template"
 plugin_path="disciple-tools-plugin-starter-template"
 plugin_post_type="starter_post_type"
-foo="Yusss!"
 
 
 echo -e "\nReplacing string 'Plugin Starter Template'... to '$plugin_name'."
@@ -14,17 +13,17 @@ grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter 
 #sed -i -- "s/Plugin Starter Template/$plugin_name/g" *.php
 
 echo -e "Replacing string 'Disciple_Tools_Plugin_Starter_Template'... to '$plugin_class'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_class/g"
+grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Disciple_Tools_Plugin_Starter_Template/$plugin_class/g"
 
 
 echo -e "Replacing string 'disciple_tools_plugin_starter_template'... to '$plugin_function'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_function/g"
+grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/disciple_tools_plugin_starter_template/$plugin_function/g"
 
 echo -e "Replacing string 'disciple-tools-plugin-starter-template'... to '$plugin_path'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_path/g"
+grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/disciple-tools-plugin-starter-template/$plugin_path/g"
 
 echo -e "Replacing string 'starter_post_type'... to '$plugin_post_type'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_post_type/g"
+grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/starter_post_type/$plugin_post_type/g"
 
 mv disciple-tools-plugin-starter-template.php "$plugin_path.php"
 

592f14372bde51f147fcfb5f06facb3751cad5d3
Prykon <dariomanoukian@gmail.com>
2022-05-09 19:21:45 -0300

    Softcoded test variable

--- customize.sh
+++ customize.sh
@@ -9,21 +9,21 @@ plugin_post_type="starter_post_type"
 
 
 echo -e "\nReplacing string 'Plugin Starter Template'... to '$plugin_name'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_name/g"
+grep -rl "Plugin Starter Template" | grep -Ev customize.sh | xargs sed -i -- "s/Plugin Starter Template/$plugin_name/g"
 #sed -i -- "s/Plugin Starter Template/$plugin_name/g" *.php
 
 echo -e "Replacing string 'Disciple_Tools_Plugin_Starter_Template'... to '$plugin_class'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/Disciple_Tools_Plugin_Starter_Template/$plugin_class/g"
+grep -rl "Disciple_Tools_Plugin_Starter_Template" | grep -Ev customize.sh | xargs sed -i -- "s/Disciple_Tools_Plugin_Starter_Template/$plugin_class/g"
 
 
 echo -e "Replacing string 'disciple_tools_plugin_starter_template'... to '$plugin_function'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/disciple_tools_plugin_starter_template/$plugin_function/g"
+grep -rl "disciple_tools_plugin_starter_template" | grep -Ev customize.sh | xargs sed -i -- "s/disciple_tools_plugin_starter_template/$plugin_function/g"
 
 echo -e "Replacing string 'disciple-tools-plugin-starter-template'... to '$plugin_path'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/disciple-tools-plugin-starter-template/$plugin_path/g"
+grep -rl "disciple-tools-plugin-starter-template" | grep -Ev customize.sh | xargs sed -i -- "s/disciple-tools-plugin-starter-template/$plugin_path/g"
 
 echo -e "Replacing string 'starter_post_type'... to '$plugin_post_type'."
-grep -rl Foo\ Barz2 | grep -Ev customize.sh | xargs sed -i -- "s/starter_post_type/$plugin_post_type/g"
+grep -rl "starter_post_type" | grep -Ev customize.sh | xargs sed -i -- "s/starter_post_type/$plugin_post_type/g"
 
 mv disciple-tools-plugin-starter-template.php "$plugin_path.php"
 
